61c9fdc09af2614f1503b07ee761666d9e6bb83e
ProyectoCBA72 <cbaproyecto72@gmail.com>
2024-06-17 16:49:56 -0500

    Update settings.py

--- cba/settings.py
+++ cba/settings.py
@@ -151,7 +151,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORS_ORIGIN_ALLOW_ALL = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
