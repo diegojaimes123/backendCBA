8be5c21f117396d1a617a53a61d52aa123e8cfaf
ProyectoCBA72 <cbaproyecto72@gmail.com>
2024-06-17 10:27:40 -0500

    Update settings.py

--- cba/settings.py
+++ cba/settings.py
@@ -19,26 +19,27 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=_(sfj5o_6*3_$@60g44!%hp(e5n(#dau5l!b754540j8a#%2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEbUG", "false").lower() == "true"
 
 ALLOWED_HOSTS = ['*']
 
-env = environ.Env()
-environ.Env.read_env()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cbaproyecto72@gmail.com'
-EMAIL_HOST_PASSWORD = 'emgi dwut xyws hmev'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 # Application definition
