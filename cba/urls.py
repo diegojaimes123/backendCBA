f3b02c2a3cb57a7f579ebb9e9cd49d54fb5d93eb
ProyectoCBA72 <cbaproyecto72@gmail.com>
2024-06-17 11:40:14 -0500

    Update urls.py

--- cba/urls.py
+++ cba/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api.urls import *
 from cba import settings
 from django.conf.urls.static import static
@@ -26,5 +26,3 @@ urlpatterns = [
     
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
